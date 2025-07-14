cf0cee17e0a28853bb0721e2435cd10fa2bc7462
Issa-barry <issabarry67@gmail.com>
2025-07-14 16:30:39 +0200

    etape 2

--- src/app/demo/components/ventes/commande/commande-detail/commande-detail.component.ts
+++ src/app/demo/components/ventes/commande/commande-detail/commande-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ConfirmationService, MessageService } from 'primeng/api';
+import { UpdateCommandeDto } from 'src/app/demo/models/commande-update.dto';
 import { Commande } from 'src/app/demo/models/commande.model';
 import { Contact } from 'src/app/demo/models/contact';
 import { Produit } from 'src/app/demo/models/produit.model';
@@ -105,5 +106,86 @@ lignes: {
     this.totalBrut = brut;
     this.totalCommande = brut - this.reduction;
   }
+
+  ///partie3
+  editProduct(): void {
+  this.isEditMode = true;
+  this.titrePage = 'Modification de la commande : ' + this.commande.numero;
+}
+   
+  cancelEdit(): void {
+  this.isEditMode = false;
+  this.loadCommande(); // recharge les données d'origine
+  this.apiErrors = {};
+  this.errorMessage = '';
+}
+//    saveCommande(): void {
+//   // À implémenter selon ton service updateCommande()
+//   this.messageService.add({
+//     severity: 'success',
+//     summary: 'Succès',
+//     detail: 'Commande enregistrée avec succès.'
+//   });
+
+//   this.isEditMode = false;
+//   this.loadCommande();
+// }
+
+saveCommande(): void {
+  const payload: UpdateCommandeDto = {
+    contact_id: this.commande.contact?.id!,
+    reduction: this.reduction,
+    lignes: this.lignes.map(ligne => ({
+      produit_id: ligne.produit!.id!,
+      quantite: ligne.quantite,
+      prix_vente: ligne.prix_vente
+    }))
+  };
+
+//   this.commandeService.updateCommande(this.commande.id, payload).subscribe({
+//     next: () => {
+//       this.messageService.add({
+//         severity: 'success',
+//         summary: 'Succès',
+//         detail: 'Commande mise à jour avec succès.'
+//       });
+//       this.isEditMode = false;
+//       this.loadCommande();
+//     },
+//     error: (err) => {
+//       this.apiErrors = err.error?.data || {};
+//       this.errorMessage = err.error?.message || 'Erreur lors de la mise à jour';
+//     }
+//   });
+}
+
+ confirmDelete(): void {
+  this.confirmationService.confirm({
+    message: 'Êtes-vous sûr de vouloir supprimer cette commande ?',
+    header: 'Confirmation',
+    icon: 'pi pi-exclamation-triangle',
+    accept: () => {
+      this.commandeService.deleteCommande(this.commande.id!).subscribe({
+        next: () => {
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Succès',
+            detail: 'Commande supprimée avec succès.'
+          });
+          this.router.navigate(['/dashboard/ventes/commande']);
+        },
+        error: (err) => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erreur',
+            detail: 'La suppression a échoué.'
+          });
+          console.error(err);
+        }
+      });
+    }
+  });
+}
+
 
 }
